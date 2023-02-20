04d61466e2b3a8efa4cc4af898503eed0a724065
Giaco21 <germinario.giacomo@gmail.com>
2023-02-20 12:20:54 +0100

    Small Change

--- 20.02.23/Fibonacci Exercises/FibRecursion.java
+++ 20.02.23/Fibonacci Exercises/FibRecursion.java
@@ -14,7 +14,7 @@ public class FibRecursion{
     }    
     public static void main(String args[]){    
      int count=0;
-     System.out.println("Prego inserisca il numero a cui far terminare la serie di Fibonacci\n:");  
+     System.out.println("Prego inserisca il numero a cui far terminare la serie di Fibonacci: \n");  
      count = contator.nextInt();
      System.out.print(n1+" "+n2);//print 0 and 1    
      printFibonacci(count-2);//n-2 perchè i primi 2 numeri sono già inseriti di base ( 0 e 1)  
