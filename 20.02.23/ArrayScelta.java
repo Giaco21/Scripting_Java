05d1673c81ac40ccb9f1ce94d6623f25354c17ab
Giaco21 <germinario.giacomo@gmail.com>
2023-02-21 09:22:28 +0100

    MOdifiche file Array

--- 20.02.23/ArrayScelta.java
+++ 20.02.23/ArrayScelta.java
@@ -3,56 +3,73 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayScelta {
+        //Variabili e Scanners
+    static String s;
+    static int selezione,n;
+   
+    static Scanner myObj=new Scanner(System.in);
+    static Scanner myObj1=new Scanner(System.in);
+   
+    static Integer arrN[] = {1,2,3,4,5,6};
+    static String arr[]={"Ciao","Hello","Salut"};
+   
+    static boolean uscita=false;
     public static void main(String[] arg){
-//dichiarazioni variabili e scanner
-    String s;
-    Scanner myObj=new Scanner(System.in);
-    Scanner myObj1=new Scanner(System.in);
-    Integer arrN[] = {1,2,3,4,5,6};
-    String arr[]={"Ciao","Hello","Salut"};
-    boolean uscita=false;
-    int selezione,n;
-    //inizio ciclo programma
-    do{
-        //prima selezione
-        System.out.println("Vuoi inserire o visualizzare l'array? \ninserire:1   \nVisuaizzare:2  \nUscire:0");
-        selezione=myObj.nextInt();
-        switch(selezione){
-            
-            case 1://inserimento con differenziazione int stringa
-                System.out.println("1=String  2=int");
-                selezione=myObj.nextInt();
-                if(selezione==1){
-                    System.out.println("Inserire la stringa");
-                    s=myObj1.nextLine();
-                    ArrayList<String> arrayListS = new ArrayList<String>(Arrays.asList(arr));
-                    arrayListS.add(s);
-                    arr = arrayListS.toArray(arr);
-                }else{
-                    System.out.println("inserire l'int");
-                    n=myObj1.nextInt();
-                    ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(arrN));
-                    arrayList.add(n);
-                    arrN = arrayList.toArray(arrN);
-                }
-                break;
+        Menu();
+    }
+
+
+    public static void Menu(){
+
+        
+    //Inizio ciclo col do while (Verrà eseguito ALMENO una volta)
+
+        do{
+
+            //prima selezione se inserire / visualizare / uscire
+    
+            System.out.println("Vuoi inserire o visualizzare l'array?\n [1]INSERIMENTO\n [2]VISUALIZZAZIONE\n [0]USCITA ");
+            System.out.println("NOTA BENE! SE SI SCEGLIE DI INSERIRE IL VALORE INSERITO SARA' INSERITO NELL'ULTIMA POSIZIONE DELL'ARRAY\n");
+            selezione=myObj.nextInt();
+            switch(selezione){
                 
-            case 2://visualizzazione con differenziazione int stringa
-                System.out.println("1=String  2=int");
-                selezione=myObj.nextInt();
-                if(selezione==1){
-                    System.out.println("l'array stringa è\n"+Arrays.toString(arr));
-                }else{
-                    System.out.println("l'array stringa è\n"+Arrays.toString(arrN));
-                }
+                //case 1 se inserire sull'array di stringa o interi/ 1=string 2=int
+                case 1:
+                    System.out.println("1=String  2=int");
+                    selezione=myObj.nextInt();
+                    if(selezione==1){
+                        System.out.println("Inserire la stringa");
+                        s=myObj1.nextLine();
+                        ArrayList<String> arrayListS = new ArrayList<String>(Arrays.asList(arr));
+                        arrayListS.add(s);
+                        arr = arrayListS.toArray(arr);
+                    }else{
+                        System.out.println("inserire l'int");
+                        n=myObj1.nextInt();
+                        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(arrN));
+                        arrayList.add(n);
+                        arrN = arrayList.toArray(arrN);
+                    }
+                    break;
+                //case 2 se visualizzare l'array di stringa o intero/ 1=string 2=int
+                case 2:
+                    System.out.println("1=String  2=int");
+                    selezione=myObj.nextInt();
+                    if(selezione==1){
+                        System.out.println("l'array stringa è\n"+Arrays.toString(arr));
+                    }else{
+                        System.out.println("l'array stringa è\n"+Arrays.toString(arrN));
+                    }
+                    break;
+                    
+                //uscita dal programma
+                case 0:
+                uscita=true;
                 break;
-                
-            case 0://uscita
-            uscita=true;
-            break;
-            default://controllo sull'inserimento
-            System.out.println("Inserimento errato");
-        }
-    }while(uscita!=true);
+                //Se non inserisce nessuno dei 3 numeri da inserimento errato e ripete la selezione iniziale
+                default:
+                System.out.println("Inserimento errato");
+            }
+        }while(uscita!=true); // la variabile la controlla se vera sempre, ma solo nel caso del case: 0 viene resa vera e quindi rompe il loop
     }
 }
