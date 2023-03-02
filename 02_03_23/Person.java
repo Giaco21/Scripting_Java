8cd24b2416a02b644f2a9b245ed89f72b5e3ec59
Giaco21 <germinario.giacomo@gmail.com>
2023-03-02 15:30:15 +0100

    Added 1st Getter Setter

---
+++ 02_03_23/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    private String name;
+
+    public  Person(String nome){
+
+        this.name = nome;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+}
