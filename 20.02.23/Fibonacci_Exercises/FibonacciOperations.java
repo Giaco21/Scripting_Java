c1c36f87d9ea44849c7ab5ae69a869641301a94d
Giaco21 <germinario.giacomo@gmail.com>
2023-03-07 11:12:40 +0100

    Modify Method inputSelezione() in Fibonacci

--- 20.02.23/Fibonacci_Exercises/FibonacciOperations.java
+++ 20.02.23/Fibonacci_Exercises/FibonacciOperations.java
@@ -4,66 +4,66 @@ public class FibonacciOperations {
 
     static MenuFibonacci myMenu = new MenuFibonacci();
 
-public static void FibonacciGenerator() {
+    public static void FibonacciGenerator() {
 
-    int num = myMenu.inputSelezione("Inserisci il numero di termini di Fibonacci da generare: ");
+        int num = myMenu.inputSelezione("Inserisci il numero di termini di Fibonacci da generare: ");
 
-    int n1 = 0, n2 = 1, n3, i;
-    System.out.print(n1 + " " + n2);// printing 0 and 1
+        int n1 = 0, n2 = 1, n3, i;
+        System.out.print(n1 + " " + n2);// printing 0 and 1
 
-    for (i = 2; i < num; ++i)// loop starts from 2 because 0 and 1 are already printed
-    {
-        n3 = n1 + n2;
-        System.out.print(" " + n3);
-        n1 = n2;
-        n2 = n3;
-    }
-    System.out.println();
-}
-
-public static void FibonacciGeneratorEvenNumber() {
-
-    int num = myMenu.inputSelezione("Inserisci il numero di termini di Fibonacci dispari da generare: ");
-
-    int n1 = 0, n2 = 1, n3, i, count = 0;
-    System.out.print(n1 + " " + n2);// printing 0 and 1
-    count = 2;
-
-    for (i = 2; count < num; ++i)// loop starts from 2 because 0 and 1 are already printed
-    {
-        n3 = n1 + n2;
-        n1 = n2;
-        n2 = n3;
-        if (n3 % 2 == 0)
-            continue;
-        count++;
-        System.out.print(" " + n3);
-    }
-    System.out.println();
-}
-
-public static void FinderFibonacci() {
-
-    int num = myMenu.inputSelezione("Inserisci il numero di cui vuoi trovare la posizione in Fibonacci: ");
-
-    int n1 = 0, n2 = 1, n3, i = 0;
-
-    if (num == n1 || num == n2) {
-        System.out.println(num + " è il " + (num == n1 ? "1°" : "2°") + " numero di Fibonacci");
-        return;
+        for (i = 2; i < num; ++i)// loop starts from 2 because 0 and 1 are already printed
+        {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+        System.out.println();
     }
 
-    do {
-        n3 = n1 + n2;
-        n1 = n2;
-        n2 = n3;
-        i++;
-        if (n3 == num) {
-            System.out.println(num + " è il " + (i + 2) + "° numero di Fibonacci");
+    public static void FibonacciGeneratorEvenNumber() {
+
+        int num = myMenu.inputSelezione("Inserisci il numero di termini di Fibonacci dispari da generare: ");
+
+        int n1 = 0, n2 = 1, n3, i, count = 0;
+        System.out.print(n1 + " " + n2);// printing 0 and 1
+        count = 2;
+
+        for (i = 2; count < num; ++i)// loop starts from 2 because 0 and 1 are already printed
+        {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            if (n3 % 2 == 0)
+                continue;
+            count++;
+            System.out.print(" " + n3);
+        }
+        System.out.println();
+    }
+
+    public static void FinderFibonacci() {
+
+        int num = myMenu.inputSelezione("Inserisci il numero di cui vuoi trovare la posizione in Fibonacci: ");
+
+        int n1 = 0, n2 = 1, n3, i = 0;
+
+        if (num == n1 || num == n2) {
+            System.out.println(num + " è il " + (num == n1 ? "1°" : "2°") + " numero di Fibonacci");
             return;
         }
-    } while (n3 <= num);
 
-    System.out.println(num + " non è un numero di Fibonacci");
-}
+        do {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            i++;
+            if (n3 == num) {
+                System.out.println(num + " è il " + (i + 2) + "° numero di Fibonacci");
+                return;
+            }
+        } while (n3 <= num);
+
+        System.out.println(num + " non è un numero di Fibonacci");
+    }
 }
--- 20.02.23/Fibonacci_Exercises/MenuFibonacci.java
+++ 20.02.23/Fibonacci_Exercises/MenuFibonacci.java
@@ -3,32 +3,36 @@ package Fibonacci_Exercises;
 import java.util.Scanner;
 
 public class MenuFibonacci {
-    private Scanner sc = new Scanner(System.in);
 
     public void menu() {
 
         boolean continua = true;
 
         do {
-            // System.out.println("Prego inserisca una scelta\n [1]Genera Fibonacci\n
-            // [2]Genera Fibonacci solo per dispari\n [3]Trova in Fibonacci\n [4]Esci\n");
-            int sc = inputSelezione("Prego inserisca una scelta\n [1]Genera Fibonacci\n [2]Genera Fibonacci solo per dispari\n [3]Trova in Fibonacci\n [4]Esci\n");
+            int sc = inputSelezione(
+                    "Prego inserisca una scelta\n [1]Genera Fibonacci\n [2]Genera Fibonacci solo per dispari\n [3]Trova in Fibonacci\n [4]Esci\n");
 
             switch (sc) {
 
                 case 1:
                     FibonacciOperations.FibonacciGenerator();
+
                     break;
+
                 case 2:
                     FibonacciOperations.FibonacciGeneratorEvenNumber();
+
                     break;
+
                 case 3:
-                    // FibonacciFinderNmr();
                     FibonacciOperations.FinderFibonacci();
+
                     break;
+
                 case 4:
                     System.out.println("Arrivederci!");
                     continua = false;
+
                     return;
 
                 default:
@@ -38,11 +42,12 @@ public class MenuFibonacci {
     }
 
     public int inputSelezione(String inputMessage) {
+        Scanner sc = new Scanner(System.in);
         System.out.println(inputMessage);
         int num = sc.nextInt();
-        sc.nextLine(); // consuma il carattere di fine linea lasciato dal metodo nextInt()
+        sc.close();
         return num;
+
     }
 
-    
 }
