148185afdf66c9596cf86caa64bd9b7036673eb1
Giaco21 <germinario.giacomo@gmail.com>
2023-03-09 11:47:40 +0100

    Modify protected prezzo totale

--- 09_03_23/Mattina/Ordinazione.java
+++ 09_03_23/Mattina/Ordinazione.java
@@ -1,23 +1,26 @@
 package Mattina;
-
 import java.util.ArrayList;
 
-public class Ordinazione {
+public class Ordinazione extends ArrayList<Piatto> {
     private ArrayList<Piatto> piatti;
-    
+    protected double totale = 0;
+
     public Ordinazione() {
         piatti = new ArrayList<Piatto>();
     }
-    
+
     public void aggiungiPiatto(Piatto piatto) {
         piatti.add(piatto);
     }
-    
+
     public double calcolaTotale() {
-        double totale = 0;
         for (Piatto piatto : piatti) {
             totale += piatto.getPrezzo();
         }
+        return totale;
+    }
+    
+    public double getTotale() {
         return totale;
     }
 }
