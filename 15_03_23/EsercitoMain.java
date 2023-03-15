5b4d4a09a661ce4a9e8f0c5962f8a89c36e426b8
Giaco21 <germinario.giacomo@gmail.com>
2023-03-15 15:19:50 +0100

    Modificati i controlli della classe esercito

--- 15_03_23/EsercitoMain.java
+++ 15_03_23/EsercitoMain.java
@@ -9,19 +9,24 @@ public class EsercitoMain {
         boolean running = true;
         while (running) {
             stampaMenu();
-            int scelta = scanner.nextInt();
-            switch (scelta) {
-                case 1:
-                    aggiungiSoldato();
-                    break;
-                case 2:
-                    visualizzaSoldati();
-                    break;
-                case 3:
-                    running = false;
-                    break;
-                default:
-                    System.out.println("Scelta non valida!");
+            int scelta;
+            try {
+                scelta = Integer.parseInt(scanner.next());
+                switch (scelta) {
+                    case 1:
+                        aggiungiSoldato();
+                        break;
+                    case 2:
+                        visualizzaSoldati();
+                        break;
+                    case 3:
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Scelta non valida!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Inserisci un numero corretto!");
             }
         }
         System.out.println("Arrivederci!");
@@ -40,25 +45,37 @@ public class EsercitoMain {
         String nome = scanner.next();
         System.out.print("Cognome: ");
         String cognome = scanner.next();
-        System.out.print("Anno di nascita: ");
-        String dataNascita = scanner.next();
+        int dataNascita = chiediAnno("Anno di nascita");
         boolean forzeSpeciali = chiediForzeSpeciali();
         if (forzeSpeciali) {
             String specializzazione = chiediSpecializzazione();
-            String annodoInizio = chiediAnnodoInizio();
-            esercito.creaSoldato(nome + " " + cognome, dataNascita, true);
+            int annoInizio = chiediAnnoInizio(dataNascita);
+            esercito.creaSoldato(nome + " " + cognome, Integer.toString(dataNascita), true);
             Esercito.SoldatoForzeSpeciali soldato = (Esercito.SoldatoForzeSpeciali) esercito.getSoldati().get(esercito.getNumeroSoldati() - 1);
             soldato.setSpecializzazione(specializzazione);
-            soldato.setAnnodoInizio(annodoInizio);
+            soldato.setAnnodoInizio(Integer.toString(annoInizio));
         } else {
-            esercito.creaSoldato(nome + " " + cognome, dataNascita, false);
+            esercito.creaSoldato(nome + " " + cognome, Integer.toString(dataNascita), false);
         }
         System.out.println("Soldato aggiunto con successo!");
     }
 
     private static boolean chiediForzeSpeciali() {
-        System.out.print("È nelle forze speciali? (S/N): ");
-        return scanner.next().equalsIgnoreCase("S");
+        while (true) {
+            try {
+                System.out.print("È nelle forze speciali? (S/N): ");
+                String risposta = scanner.next();
+                if (risposta.equalsIgnoreCase("S")) {
+                    return true;
+                } else if (risposta.equalsIgnoreCase("N")) {
+                    return false;
+                } else {
+                    System.out.println("Inserisci S o N!");
+                }
+            } catch (Exception e) {
+                System.out.println("Inserimento non valido!");
+            }
+        }
     }
 
     private static String chiediSpecializzazione() {
@@ -66,11 +83,40 @@ public class EsercitoMain {
         return scanner.next();
     }
 
-    private static String chiediAnnodoInizio() {
-        System.out.print("Anno di inizio attività: ");
-        return scanner.next();
+    private static int chiediAnno(String prompt) {
+        while (true) {
+            try {
+                System.out.print(prompt + ": ");
+                int anno = scanner.nextInt();
+                if (anno < 1900) {
+                    throw new IllegalArgumentException("L'anno non può essere inferiore a 1900.");
+                }
+                return anno;
+            } catch (Exception e) {
+                System.out.println("Inserire un anno valido!");
+                scanner.nextLine();
+            }
+        }
     }
-
+    
+    private static int chiediAnnoInizio(int annoNascita) {
+        while (true) {
+            try {
+                System.out.print("Anno di inizio attività: ");
+                int annoInizio = scanner.nextInt();
+                if (annoInizio <= annoNascita) {
+                    throw new IllegalArgumentException("L'anno di inizio attività deve essere successivo all'anno di nascita.");
+                }
+                if (annoInizio - annoNascita < 18) {
+                    throw new IllegalArgumentException("L'anno di inizio attività deve essere almeno 18 anni dopo l'anno di nascita.");
+                }
+                return annoInizio;
+            } catch (Exception e) {
+                System.out.println("Inserire un anno valido!");
+                scanner.nextLine();
+            }
+        }
+    }
     private static void visualizzaSoldati() {
         int numeroSoldati = esercito.getNumeroSoldati();
         if (numeroSoldati == 0) {
